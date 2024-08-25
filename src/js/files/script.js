d9385f66348e99c9dbb9548b25cb634467d0c942
PashKo911 <rabota.pashok@gmail.com>
2024-08-25 12:15:39 +0300

    added animations

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,14 @@
 // Підключення функціоналу "Чертоги Фрілансера"
-import { isMobile } from "./functions.js";
+import { isMobile, applyAnimationDelay } from './functions.js'
 // Підключення списку активних модулів
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js'
+
+// const paths = document.querySelectorAll('.hero__title svg path')
+
+// paths.forEach((path) => {
+// 	path.style.strokeDasharray = path.getTotalLength()
+// 	path.style.strokeDashoffset = path.getTotalLength()
+// 	console.log(path.getTotalLength())
+// })
+
+// applyAnimationDelay(elements, delayBetween = 0.2, generalDelay = 0)
